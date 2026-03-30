e33d0d92a65bfe44b4e51f692e080effcae147a3
sevgi72 <sevgimf@code.edu.az>
2026-03-30 21:34:14 +0400

    initial

---
+++ EternaApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EternaApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
